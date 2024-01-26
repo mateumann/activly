2a854e58b2d7ea0c426afdd42bbc768e5942c088
Mateusz Neumann <46427731+mateumann@users.noreply.github.com>
2024-01-26 19:22:48 +0100

    Add more tests (#29)
    * Update workflows' configuration
    
    * Add test for UserService.ListUsers()
    
    * Silly test for UserService.Ready()
      (because the implementation is also kind of silly)

--- core/services/user_test.go
+++ core/services/user_test.go
@@ -1,13 +1,18 @@
 package services
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/mateumann/activly/core/domain"
 )
 
+var errBang = errors.New("bang")
+
 type mockUserRepository struct {
-	err error
+	users []*domain.User
+	err   error
 }
 
 func (m *mockUserRepository) Save(_ domain.User) error {
@@ -15,7 +20,7 @@ func (m *mockUserRepository) Save(_ domain.User) error {
 }
 
 func (m *mockUserRepository) ListUsers() ([]*domain.User, error) {
-	return nil, m.err
+	return m.users, m.err
 }
 
 func TestNewUserService(t *testing.T) {
@@ -33,6 +38,78 @@ func TestNewUserService(t *testing.T) {
 
 		if userService.repo != nil {
 			t.Errorf("UserService.repo = %v, wanted nil", userService.repo)
+		}
+	})
+}
+
+func TestUserService_ListUsers(t *testing.T) {
+	t.Run("NilUsersAnError", func(t *testing.T) {
+		repo := &mockUserRepository{nil, errBang}
+		userService := NewUserService(repo)
+
+		users, err := userService.ListUsers()
+		if err == nil {
+			t.Errorf("UserService.ListUsers() error = nil, wanted an %v", errBang)
+		}
+
+		if users != nil {
+			t.Errorf("UserService.ListUsers() = %v, wanted nil", users)
+		}
+	})
+
+	t.Run("SomeUsersAnError", func(t *testing.T) {
+		repoUsers := []*domain.User{{Name: "Alice"}, {Name: "Bob"}}
+		repo := &mockUserRepository{repoUsers, errBang}
+		userService := NewUserService(repo)
+
+		repoUsers, err := userService.ListUsers()
+		if err == nil {
+			t.Errorf("UserService.ListUsers() error = nil, wanted an %v", errBang)
+		}
+
+		if repoUsers != nil {
+			t.Errorf("UserService.ListUsers() = %v, wanted nil", repoUsers)
+		}
+	})
+
+	t.Run("SomeUsersNoError", func(t *testing.T) {
+		repoUsers := []*domain.User{{Name: "Alice"}, {Name: "Bob"}}
+		repo := &mockUserRepository{repoUsers, nil}
+		userService := NewUserService(repo)
+
+		users, err := userService.ListUsers()
+		if err != nil {
+			t.Errorf("UserService.ListUsers() error = %v, wanted nil", err)
+		}
+
+		if !reflect.DeepEqual(users, repoUsers) {
+			t.Errorf("UserService.ListUsers() = %v, wanted %v", users, repoUsers)
+		}
+	})
+
+	t.Run("NoUsersNoError", func(t *testing.T) {
+		var repoUsers []*domain.User
+		repo := &mockUserRepository{repoUsers, nil}
+		userService := NewUserService(repo)
+
+		users, err := userService.ListUsers()
+		if err != nil {
+			t.Errorf("UserService.ListUsers() error = %v, wanted nil", err)
+		}
+
+		if !reflect.DeepEqual(users, repoUsers) {
+			t.Errorf("UserService.ListUsers() = %v, wanted %v", users, repoUsers)
+		}
+	})
+}
+
+func TestUserService_Ready(t *testing.T) {
+	t.Run("AlwaysTrue", func(t *testing.T) {
+		userService := NewUserService(nil)
+
+		ready := userService.Ready()
+		if ready != true {
+			t.Errorf("UserService.Ready() = %v, want true", ready)
 		}
 	})
 }
